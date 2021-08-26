c15e42d9c682d9cb2f122857b5daef1de582ede4
Djomeni <wilfrieddjidjo@gmail.com>
2021-08-26 10:04:09 +0200

    first routing

--- VisioFantasiapp/src/app/app-routing.module.ts
+++ VisioFantasiapp/src/app/app-routing.module.ts
@@ -1,7 +1,44 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FiltersComponent } from './filters/filters.component';
+import { LoggedPageComponent } from './logged-page/logged-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { PublicPageComponent } from './public-page/public-page.component';
+// const routes: Routes = []
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
+  {
+        path: 'login',
+        component: LoginPageComponent,
+  },
+  {
+        path: 'logged',
+        component: LoggedPageComponent,
+  },
+  {
+        path: 'filters',
+        component: FiltersComponent,
+  },
+  // {
+  //   path: '',
+  //   component: LoggedPageComponent,
+  //   children: [
+  //     {
+  //       path: ':annee',
+  //       data: { breadcrumb: 'Annee' },
+  //       children: [
+  //         {
+  //           path: '',{}
+  //           data: { breadcrumb: null },
+  //           component: DataViewComponent,
+  //         }
+  //       ],
+  //     },
+  //   ],
+  // },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
