4077ef88e89d9b2a582638af3e0b3f52c3f458a3
Djomeni <wilfrieddjidjo@gmail.com>
2021-08-30 17:43:45 +0200

    navigations changes

--- src/app/filters/filters.component.ts
+++ src/app/filters/filters.component.ts
@@ -13,6 +13,10 @@ interface listLev {
   name: string[];
   id: number[];
 }
+interface lev {
+  name: string;
+  id: number;
+}
 @Component({
   selector: 'app-filters',
   templateUrl: './filters.component.html',
@@ -26,35 +30,17 @@ export class FiltersComponent implements OnInit {
   selectList!: listDash;
   currentLev: any;
   levelName: string = '';
-  superLevel!: {
-    name: string;
-    id: string;
-  };
+  superLevel!: lev
   value: number = 0;
   subLevels!: listLev;
   sort: number = 0;
+  
   ngOnInit(): void {
     this.blockToShow();
   }
-
+  
   private blockToShow() {
-    combineLatest([
-      this.filtersState.arraySubject,
-      this.filtersState.stateSubject,
-    ]).subscribe(([currentsArrays, currentStates]) => {
-      this.listLevel = currentsArrays.levelArray.currentLevel;
-      this.subLevels = currentsArrays.levelArray.subLevel;
-      this.listDashboard = currentsArrays.dashboardArray;
-      this.superLevel = currentsArrays.levelArray.superLevel.name;
-      this.currentLev = currentStates.States.Level;
-      // console.debug('la liste des dashBoard', this.listDashboard)
-      this.levelName =
-        currentStates.States.level.label == ''
-          ? 'National'
-          : currentStates.States.level.label;
-
-      this.viewList = this.subLevels;
-    });
+   
   }
 
   private destroy$: Subject<void> = new Subject<void>();
@@ -64,15 +50,14 @@ export class FiltersComponent implements OnInit {
     this.destroy$.complete();
   }
 
-  updateBlock(level: listLev) {
+  updateBlock(level: lev) {
     console.debug(this.subLevels);
-    this.viewList = level;
+    this.updateState(level.id)
   }
 
   updateState(indexLev?: number, indexDash?: number) {
     if (indexLev) {
       this.filtersState.updateState(this.subLevels.id[indexLev - 1]);
-      this.blockToShow();
       console.debug('index list', this.subLevels.id[indexLev]);
     } else if (indexDash)
       this.filtersState.updateState(this.listDashboard.id[indexDash]);
