b06e3773aa05d8943549d634b1c96ea9b6b0f11f
Djomeni <wilfrieddjidjo@gmail.com>
2021-09-09 06:36:18 +0200

    add first widget

--- src/app/widgets/simple-pie/simple-pie.component.ts
+++ src/app/widgets/simple-pie/simple-pie.component.ts
@@ -16,88 +16,87 @@ export class SimplePieComponent implements OnInit {
   }
   private data = nationalP2CD.tableauHaut;
   private svg: any = {};
-  private margin = 50;
+  private margin = 150;
   private width = 750;
   private height = 600;
 
   private radius = Math.min(this.width, this.height) / 2 - this.margin;
   private colors: any = {};
-  private legendRectSize = 18;                                  
-  private legendSpacing = 4; 
+  private legendRectSize = 18;
+  private legendSpacing = 9;
 
   createSvg(): void {
     this.svg = d3
-      .select('figure#pie')
+      .select('div#container')
       .append('svg')
-      .attr('width', this.width)
-      .attr('height', this.height)
+      .attr('preserveAspectRatio', 'xMinYMin meet')
+      .attr('viewBox', '0 0 ' + (this.width+400) + ' ' + this.height)
+      .classed("svg-content", true)
       .append('g')
       .attr(
         'transform',
         'translate(' + this.width / 2 + ',' + this.height / 2 + ')'
-      );
+      )
   }
   createColors(): void {
     this.colors = d3
       .scaleOrdinal()
       .domain(this.data.map((d) => d.valeur.toString()))
-      .range(['#c7d3ec', '#a5b8db', '#879cc4', '#677795']);
+      .range(['#DC6206', '#888888', '#E1962A', '#DEDEDE']);
   }
   drawChart(): void {
     // Compute the position of each group on the pie:
     const pie = d3.pie<any>().value((d: any) => Number(d.valeur));
 
-    // Build the pie chart
     this.svg
       .selectAll('pieces')
       .data(pie(this.data))
       .enter()
       .append('path')
-      .attr('d', d3.arc().innerRadius(0).outerRadius(this.radius))
+      .attr('d', d3.arc().innerRadius(0).outerRadius(this.radius+30))
       .attr('fill', (d: any, i: number) => this.colors(i))
-      .attr('stroke', 'blue')
-      .style('stroke-width', '1px');
+      .attr('transform', 'translate(' + -50 + ',' + -80 + ')');
 
     // Add labels
     const labelLocation = d3.arc().innerRadius(100).outerRadius(this.radius);
 
-    this.svg
-      .selectAll('pieces')
+    // this.svg
+    //   .selectAll('pieces')
+    //   .data(pie(this.data))
+    //   .enter()
+    //   .append('text')
+    //   .text((d: any) => d.data.label)
+    //   .attr(
+    //     'transform',
+    //     (d: any) => 'translate(' + labelLocation.centroid(d) + ')'
+    //   )
+    //   .style('text-anchor', 'middle')
+    //   .style('font-size', 15);
+
+    const legend = this.svg
+      .selectAll('.legend')
       .data(pie(this.data))
       .enter()
+      .append('g')
+      .attr('class', 'legend')
+      .attr('transform', (d: any, i: number) => {
+        const height = this.legendRectSize + this.legendSpacing;
+        const horz = 2 * this.legendRectSize + 200;
+        const vert = -i * height + 100;
+        return 'translate(' + horz + ',' + vert + ')';
+      });
+
+    legend
+      .append('rect')
+      .attr('width', this.legendRectSize)
+      .attr('height', this.legendRectSize)
+      .style('fill', (d: any, i: number) => this.colors(i))
+      .style('stroke', (i: number) => this.colors(i));
+
+    legend
       .append('text')
-      .text((d: any) => d.data.label)
-      .attr(
-        'transform',
-        (d: any) => 'translate(' + labelLocation.centroid(d) + ')'
-      )
-      .style('text-anchor', 'middle')
-      .style('font-size', 15);
-
-
-
-      var legend = this.svg.selectAll('.legend')                     
-      .data(pie(this.data))                                   
-      .enter()                                                
-      .append('g')                                            
-      .attr('class', 'legend')                                
-      .attr('transform', (d:any, i:number) => {                     
-        const height = this.legendRectSize + this.legendSpacing;          
-        const offset =  height * this.colors(i).length / 2;     
-        const horz = -2 * this.legendRectSize;                       
-        const vert = i * height - offset;                       
-        return 'translate(' + horz + ',' + vert + ')';        
-      })                                                 
-
-    legend.append('rect')                                     
-      .attr('width', this.legendRectSize)                          
-      .attr('height', this.legendRectSize)                         
-      .style('fill', (d:any, i:number) => this.colors[i])                                   
-      .style('stroke', (i:number) =>this.colors[i]);                                
-      
-    legend.append('text')                                     
-      .attr('x', this.legendRectSize + this.legendSpacing)              
-      .attr('y', this.legendRectSize - this.legendSpacing)              
-      .text((d:any) => d.data.label); 
+      .attr('x', this.legendRectSize + this.legendSpacing)
+      .attr('y', this.legendRectSize - this.legendSpacing + 5)
+      .text((d: any) => d.data.label);
   }
 }
