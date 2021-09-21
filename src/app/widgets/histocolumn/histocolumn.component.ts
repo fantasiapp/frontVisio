c8aa0d5c97499729422e421712822070b5010f98
majed <majed.abdennadher@student-cs.fr>
2021-09-21 15:45:28 +0200

    style changes

--- src/app/widgets/histocolumn/histocolumn.component.ts
+++ src/app/widgets/histocolumn/histocolumn.component.ts
@@ -47,12 +47,15 @@ export class HistoColumnComponent extends BasicWidget {
         contents: (d, defaultTitleFormat, defaultValueFormat, color) => {
           const data = d[0];
           return `
-            <div class="tooltip">
+            <div class="tooltip histocolumn-tooltip">
               <span style="color:${color(data)}">${data.id}: </span>${BasicWidget.format(data.value, 3)} ${this.properties.unit}
               <div class="tooltip-tail"></div>
             </div>
           `;
         }
+      },
+      bar: {
+        sensitivity: 10
       },
       axis: {
         x: {
