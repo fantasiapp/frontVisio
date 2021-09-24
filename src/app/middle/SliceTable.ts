fdd759880f8fa6107b7b7859aa8976fe1b89f9d8
Baptiste Prévost <baptiste.prevost@telecom-paris.fr>
2021-09-24 17:49:16 +0200

    factorizing and generalizing SliceTable

--- src/app/middle/SliceTable.ts
+++ src/app/middle/SliceTable.ts
@@ -6,7 +6,7 @@ import { PDV } from "./Slice&Dice";
 export class SliceTable {
     private pdvs: any;
     private pdvFields: string[];
-    private columnData: {[key: string]: {[key: number]: string}[]} = {};
+    private idsToFields: {[key: string]: {[key: number]: string}[]} = {};
     private columnDefs: {[k: string]: any}[] = [];
     private navigationOptions: {id: any, name: any}[] = [];
     private titleData: number[] = [0,0,0];
@@ -28,18 +28,38 @@ export class SliceTable {
         'p2cd': ['siniatSells', 'totalSells'],
         'enduit':  []
     }
+
+    private customField: {[name: string]: (pdv: any) => {}} = {
+        'siniatSells': (pdv: any) => {
+            let value: number = 0;
+            for(let sale of pdv[21]) {
+                if([1,2,3].includes(sale[0]) && sale[1] === 1)
+                    value+=sale[2];
+            }
+            return value;
+        },
+        'totalSells': (pdv: any) => {
+            let value: number = 0;
+            for(let sale of pdv[21]) {
+                if([1,2,3].includes(sale[0]))
+                value+=sale[2]
+            }
+            return value;
+        },
+    }
+
     
     constructor(){
         this.pdvs = DataExtractionHelper.get('pdvs')
         this.pdvFields = DataExtractionHelper.get('structurePdv');
     
-        //Get columnData, to match id values in pdv to string values
+        //Get idsToFields, to match id values in pdv to string values
         for(let field of this.pdvFields) {
-            this.columnData[field] = DataExtractionHelper.get(field);
+            this.idsToFields[field] = DataExtractionHelper.get(field);
         }
     }
 
-    getPdvs(slice: any = {}): {[key:string]:any}[] { // Transforms pdv from lists to objects, and counts title informations
+    getPdvs(slice: any = {}, type: string): {[key:string]:any}[] { // Transforms pdv from lists to objects, and counts title informations
         if (slice !== {}){
             this.pdvs = []
             let allPdvs = DataExtractionHelper.get('pdvs');
@@ -49,53 +69,37 @@ export class SliceTable {
             }
         }
         let pdvsAsList =  [];
-        for (let pdv of this.pdvs){
-            var newPdv: {[key:string]:any} = {};
-            for(let iter = 0; iter < this.pdvFields.length; iter ++){
-                
-                //!!!!!! hardcode for P2CDtable
-                if(this.pdvFields[iter] === 'sales') {
-                    newPdv['siniatSells'] = 0;
-                    newPdv['totalSells'] = 0;
-                    let siniatSells = 0;
-                    let totalSells = 0;
-                    for(let sale of pdv[iter]){
-                        if(sale[0] === 1 || sale[0] === 2 || sale[0] === 3){ //check P2CD product id
-                            if(sale[1] === 1 ) { //check Siniat industry id
-                                siniatSells += sale[2]
-                                newPdv['siniatSells'] += sale[2]
-                            } else {
-                                totalSells += sale[2]
-                                newPdv['totalSells'] += sale[2]
-                            }
-                        }
-                    }
-                    this.titleData[1] = siniatSells;
-                    this.titleData[2] = totalSells;
+        for(let pdv of this.pdvs) {
+            var newPdv: {[key:string]:any} = {}; //concrete row of the table
+            for(let index = 0; index < Object.keys(this.getAllColumns(type)).length; index ++) {
+                let field = this.getAllColumns(type)[index]
+                if(this.idsToFields[field]) {
+                    newPdv[field] = this.idsToFields[field][pdv[index]]
+                } else if(this.specificColumns[type].includes(field)) {
+                    newPdv[field] = this.customField[field](pdv);
                 } else {
-                
-                    if(this.columnData[this.pdvFields[iter]]) {
-                        newPdv[this.pdvFields[iter]] = this.columnData[this.pdvFields[iter]][pdv[iter]]
-                    } else {
-                        newPdv[this.pdvFields[iter]] = pdv[iter];
-                    }
+                    newPdv[field] = pdv[index]
                 }
             }
-
-            pdvsAsList.push(newPdv)
+            pdvsAsList.push(newPdv);
         }
         this.titleData[0] = Object.keys(this.pdvs).length;
         return pdvsAsList;
     }
 
-    getColumnDefs(type: string, rowGroupId?: string): {}[]{ // !!! A bit hardcoded : specific to p2CDtable
+    getAllColumns(type: string) {
         let visibleColumns = this.visibleColumns[type];
         let allColumns = this.pdvFields.concat(this.specificColumns[type]);
+        return allColumns;
+    }
+
+    getColumnDefs(type: string, rowGroupId?: string): {}[]{ // !!! A bit hardcoded : specific to p2CDtable
+        let allColumns = this.getAllColumns(type);
         let columnDefs: {[key:string]: any}[] = [];
 
         for(let field of allColumns) {
             let column = {field: field, hide: true, rowGroup: false}
-            if(visibleColumns.includes(field)){
+            if(this.visibleColumns[type].includes(field)){
                 column.hide = false;
             }
             if(field === rowGroupId) {
@@ -129,7 +133,7 @@ export class SliceTable {
     getData(slice: any = {}, rowGroupId: string, type: string): {}[][]{
         let data: {}[][] = [];
         data.push(this.getColumnDefs(type, rowGroupId));
-        data.push(this.getPdvs(slice));
+        data.push(this.getPdvs(slice, type));
         data.push(this.getNavOpts(type));
         data.push(this.getTitleData());
         return data;
