1c38a1c9e79cf4e0bbb3b807924070e85ed1a7c0
Baptiste Prévost <baptiste.prevost@telecom-paris.fr>
2021-09-29 18:12:46 +0200

    Fix table titles and clean code

--- src/app/widgets/table/renderers.ts
+++ src/app/widgets/table/renderers.ts
@@ -2,56 +2,46 @@ import { Component } from "@angular/core";
 import { AgRendererComponent } from "ag-grid-angular";
 import { ICellRendererParams } from "ag-grid-community";
 
-@Component({
-  selector: 'group-name-component',
-  template: `<span>{{ this.displayValue }}</span>`,
-})
-export class GroupNameCellRenderer implements AgRendererComponent {
-  
+abstract class DefaultCellRenderer implements AgRendererComponent {
   refresh(params: ICellRendererParams): boolean {
     return true;
   }
-  displayValue: string = "";
+  agInit(params: ICellRendererParams): void {
+  }
 
+}
+
+@Component({
+  template: `<span>{{ this.displayValue }}</span>`,
+})
+export class GroupNameCellRenderer extends DefaultCellRenderer {
+  displayValue: string = "";
   agInit(params: ICellRendererParams): void {
     this.displayValue = params.value['name'] + ' PdV : ' + params.value['number']
   }
 }
 
 @Component({
-    selector: 'sales-component',
     template: `<span>{{ this.displayValue }}</span>`,
   })
-  export class RowSalesCellRenderer implements AgRendererComponent {
-    
-    refresh(params: ICellRendererParams): boolean {
-      return true;
-    }
+  export class RowSalesCellRenderer extends DefaultCellRenderer {
     displayValue: string = "";
-  
     agInit(params: ICellRendererParams): void {
       this.displayValue = Math.floor(params.value).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ')  + " m²";
     }
   }
   
   @Component({
-    selector: 'total-sales-component',
     template: `<span>{{ this.displayValue }}</span>`,
   })
-  export class GroupSalesCellRenderer implements AgRendererComponent {
-    
-    refresh(params: ICellRendererParams): boolean {
-      return true;
-    }
+  export class GroupSalesCellRenderer extends DefaultCellRenderer {
     displayValue: string = "";
-  
     agInit(params: ICellRendererParams): void {
       this.displayValue = (<any>params).text as string + Math.floor(params.value/1000).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ') as string + " km²";
     }
   }
    
   @Component({
-    selector: 'edit-component',
     template: `<img src="assets/edit.svg"/>`,
     styles:  [`:host {
         display: flex;
@@ -59,16 +49,11 @@ export class GroupNameCellRenderer implements AgRendererComponent {
         align-items: center;
       }`]
   })
-  export class EditCellRenderer implements AgRendererComponent {
-    
-    refresh(params: ICellRendererParams): boolean {
-      return true;
-    }
+  export class EditCellRenderer extends DefaultCellRenderer {
     agInit(params: ICellRendererParams): void {}
   }
   
   @Component({
-    selector: 'checkbox-component',
     template: `<input type="checkbox">`,
     styles:  [`:host {
         display: flex;
@@ -76,18 +61,10 @@ export class GroupNameCellRenderer implements AgRendererComponent {
         align-items: center;
       }`]
   })
-  export class CheckboxCellRenderer implements AgRendererComponent {
-    
-    refresh(params: ICellRendererParams): boolean {
-      return true;
-    }
-  
-    agInit(params: ICellRendererParams): void {
-    }
+  export class CheckboxCellRenderer extends DefaultCellRenderer {
   }
   
   @Component({
-    selector: 'point-feu-component',
     template: `<img *ngIf="show" src="assets/feu.svg">`,
     styles:  [`:host {
         display: flex;
@@ -95,20 +72,14 @@ export class GroupNameCellRenderer implements AgRendererComponent {
         align-items: center;
       }`]
   })
-  export class PointFeuCellRenderer implements AgRendererComponent {
-    
-    refresh(params: ICellRendererParams): boolean {
-      return true;
-    }
+  export class PointFeuCellRenderer extends DefaultCellRenderer {
     show: boolean = false;
-  
     agInit(params: ICellRendererParams): void {
       this.show = params.value;
     }
   }
 
   @Component({
-    selector: 'target-component',
     template: `
         <div [ngStyle]="{'display': 'flex', 'flex-direction': 'column', 'align-items': 'stretch'}">
             <div [ngStyle]="{'display': 'flex', 'flex-direction': 'row', 'height': '25px'}">
@@ -124,18 +95,12 @@ export class GroupNameCellRenderer implements AgRendererComponent {
             </div>
         </div>`,
   })
-  export class TargetCellRenderer implements AgRendererComponent {
-    
-    refresh(params: ICellRendererParams): boolean {
-      return true;
-    }
+  export class TargetCellRenderer extends DefaultCellRenderer {
     p2cd?: {[name: string]: number}[];
     enduit?: {[name: string]: number}[];
-
     agInit(params: ICellRendererParams): void {
         this.p2cd = params.value['p2cd'];
         this.enduit = params.value['enduit'];
-
     }
   }
 
