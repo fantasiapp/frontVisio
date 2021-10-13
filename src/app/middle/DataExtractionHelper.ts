10225593fa92a415ee9c03a92b7b05fbcac38170
Vivian Menard <vivian.menard@fantasiapp.com>
2021-10-13 18:04:14 +0200

    Fix bug on % in the description widget

--- src/app/middle/DataExtractionHelper.ts
+++ src/app/middle/DataExtractionHelper.ts
@@ -460,15 +460,16 @@ class DataExtractionHelper{
       (relevantNode.children as Node[]).map(subLevelNode => DataExtractionHelper.getTarget(subLevelNode.label, subLevelNode.id, "dnP2CD")).reduce((acc, value) => acc + value, 0),
       0],
       ciblageWidget: [number, number, number] = [0, 0, 0];
-    objectiveWidget[2] = 100 * ciblage / objectiveWidget[0];
+    objectiveWidget[2] = 0.1 * ciblage / objectiveWidget[0]; // on divise par 10 car on fait *100 pour mettre en % et /1000 pour tout mettre en km2
     if (relevantNode.label == 'France'){
       let agentNodes = (relevantNode.children as Node[]).map(drvNode => drvNode.children as Node[]).reduce((acc: Node[], list: Node[]) => acc.concat(list), []);
       ciblageWidget = [
         agentNodes.map(agentNode => DataExtractionHelper.getTarget("Secteur", agentNode.id, "volP2CD")).reduce((acc, value) => acc + value, 0),
         agentNodes.map(agentNode => DataExtractionHelper.getTarget("Secteur", agentNode.id, "dnP2CD")).reduce((acc, value) => acc + value, 0),
         0]
-        ciblageWidget[2] = 100 * ciblage / ciblageWidget[0];
+        ciblageWidget[2] = 0.1 * ciblage / ciblageWidget[0]; 
     }
+    console.log('-->', objectiveWidget)
     return [objectiveWidget, ciblageWidget];
   }
 
