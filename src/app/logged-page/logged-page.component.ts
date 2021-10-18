98149e6f586559071948a456cf29215e26333e3e
majed <majed.abdennadher@student-cs.fr>
2021-10-18 10:32:26 +0200

    repair histocolumn onresized error

--- src/app/logged-page/logged-page.component.ts
+++ src/app/logged-page/logged-page.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logged-page',
   templateUrl: './logged-page.component.html',
   styleUrls: ['./logged-page.component.css']
 })
-export class LoggedPageComponent implements OnInit {
+export class LoggedPageComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log('logged created')
+  }
+
+  @HostListener('unloaded')
+  ngOnDestroy() {
+    console.log('logged destroyed');
   }
 
 }
