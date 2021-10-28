829a96bdd04eb67752128f48408b69b45e015351
Vivian Menard <vivian.menard@fantasiapp.com>
2021-10-28 16:46:35 +0200

    begin to remove 'industrie'

--- src/app/map/info-bar/info-bar.component.ts
+++ src/app/map/info-bar/info-bar.component.ts
@@ -172,9 +172,9 @@ export class InfoBarComponent {
     this.products = PDV.getProducts() as string[];
     this.products.splice(3, this.products.length, 'P2CD')
     for(let i = 0; i<this.industries.length; i++)
-      this.industryIdToIndex[+DataExtractionHelper.getKeyByValue(DataExtractionHelper.get('industrie'), this.industries[i])!] = i+1; //first row already used
+      this.industryIdToIndex[+DataExtractionHelper.getKeyByValue(DataExtractionHelper.get('industry'), this.industries[i])!] = i+1; //first row already used
     for(let i = 0; i<this.products.length-1; i++)
-      this.productIdToIndex[+DataExtractionHelper.getKeyByValue(DataExtractionHelper.get('produit'), this.products[i])!] = i;
+      this.productIdToIndex[+DataExtractionHelper.getKeyByValue(DataExtractionHelper.get('product'), this.products[i])!] = i;
   }
 
   //make variable
