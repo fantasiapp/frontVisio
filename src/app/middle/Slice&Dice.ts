8380b35eb7c45edc1de4d3651d152315e5350993
Vivian Menard <vivian.menard@fantasiapp.com>
2021-11-10 14:24:07 +0100

    rearrange slicedice

--- src/app/middle/Slice&Dice.ts
+++ src/app/middle/Slice&Dice.ts
@@ -19,24 +19,58 @@ export class SliceDice{
   static currentNode: Node;
   constructor(private dataService: DataService){}
 
+  updateCurrentNode(node:Node){
+    SliceDice.currentNode = node;
+    SliceDice.currentSlice = PDV.slice(node);
+  }
+
   getWidgetData(axis1:string, axis2:string, indicator:string, groupsAxis1:(number|string[]), 
-      groupsAxis2:(number|string[]), percentIndicator:string, transpose=false, target=false, addConditions:[string, number[]][] = []){
+      groupsAxis2:(number|string[]), percentIndicator:string, transpose=false, 
+      target=false, addConditions:[string, number[]][] = []){
  
-    let colors; [colors, groupsAxis1, groupsAxis2] = this.computeColorsWidget(groupsAxis1, groupsAxis2);
-    let dataWidget = this.getDataFromPdvs(axis1, axis2, indicator.toLowerCase(), addConditions);
+    let colors; [colors, groupsAxis1, groupsAxis2] = this.extractInfoFromGroupAxis(
+      groupsAxis1, groupsAxis2);
+    let dataWidget = this.getDataFromPdvs(
+      axis1, axis2, indicator.toLowerCase(), addConditions);
     let km2 = !dnIndicators.includes(indicator) ? true : false, 
-      sortLines = percentIndicator !== 'classic' && axis1 != 'suiviAD';
-    dataWidget.widgetTreatement(km2, sortLines, (axis1 !== 'histoCurve') ? 'all': 'no', groupsAxis1 as string[], groupsAxis2 as string[]);
+      sortLines = (percentIndicator !== 'classic') && (axis1 != 'suiviAD');
+    dataWidget.widgetTreatement(km2, sortLines, (axis1 !== 'histoCurve') ? 'all': 'no', 
+      groupsAxis1 as string[], groupsAxis2 as string[]);
     let sum = dataWidget.getSum();
-    let targetsStartingPoint = dataWidget.getTargetStartingPoint(axis1); // it is important to collect this piece of information before changing dataWidget in percent
-    if (percentIndicator == 'classic') dataWidget.percent(); else if (percentIndicator == 'cols') dataWidget.percent(true);
-    let [rodPosition, rodPositionForCiblage, targetLevel] = this.computeTargetElements(axis1, axis2, dataWidget, sum, targetsStartingPoint, target, indicator);    
+    // it is important to collect the piece of information below before changing dataWidget in percent
+    let targetsStartingPoint = dataWidget.getTargetStartingPoint(axis1); 
+    if (percentIndicator == 'classic') dataWidget.percent(); 
+    else if (percentIndicator == 'cols') dataWidget.percent(true);
+    let [rodPosition, rodPositionForCiblage, targetLevel] = this.computeTargetElements(
+      axis1, axis2, dataWidget, sum, targetsStartingPoint, target, indicator);    
     if (typeof(sum) !== 'number') sum = 0;
-    return {data: dataWidget.formatWidgetForGraph(SliceDice.currentNode, transpose, axis1, SliceDice.currentSlice.length), sum: sum, target: rodPosition, 
-      colors: colors, targetLevel: targetLevel, ciblage: rodPositionForCiblage, threshold: this.getThresholdsForGauge(indicator)}    
+    return {
+      data: dataWidget.formatWidgetForGraph(
+        SliceDice.currentNode, transpose, axis1, SliceDice.currentSlice.length), 
+      sum: sum, target: rodPosition, colors: colors, targetLevel: targetLevel, ciblage: rodPositionForCiblage, 
+      threshold: this.getThresholdsForGauge(indicator)
+    }    
+  }
+
+  rubiksCubeCheck(indicator: string, percent:string){
+    let sortLines = percent !== 'classic';
+    let dataWidget = this.getDataFromPdvs(
+      'enseigne', 'segmentMarketing', indicator.toLowerCase(), []);
+    dataWidget.widgetTreatement(false, sortLines, 'justLines');
+    return dataWidget.numberToBool()
+  }
+
+  updateTargetLevel(newValue: number, targetLevelName: string, targetLevelId: string, 
+      volumeid: number, targetLevelStructure: string){
+    let newTargetLevel: number[] = DEH.get(targetLevelName)[targetLevelId]
+    newTargetLevel[+DEH.get(targetLevelStructure).indexOf(volumeid)] = +newValue;
+    this.dataService.updateTargetLevel(
+      newTargetLevel, targetLevelName as UpdateFields, +targetLevelId);
   }
   
-  private computeTargetElements(axis1:string, axis2:string, dataWidget:DataWidget, sum:number|number[], targetsStartingPoint:number|number[], target:boolean, indicator:string){
+  private computeTargetElements(axis1:string, axis2:string, dataWidget:DataWidget, sum:number|number[], 
+      targetsStartingPoint:number|number[], target:boolean, indicator:string){
+
     let rodPosition = undefined, rodPositionForCiblage = undefined, 
     targetLevel: {[key:string]:any} = {'name' : '', 'ids': [], 'volumeIdentifier' : '', 'structure': ''};
     if (target){
@@ -89,26 +123,31 @@ export class SliceDice{
     }
   }
 
-  private computeColorsWidget(groupsAxis1: number|string[], groupsAxis2: number|string[]){
-    if (![typeof(groupsAxis1), typeof(groupsAxis2)].includes('number')) return [undefined, groupsAxis1, groupsAxis2];
+  private extractInfoFromGroupAxis(groupsAxis1: number|string[], 
+      groupsAxis2: number|string[]){
+
+    if (![typeof(groupsAxis1), typeof(groupsAxis2)].includes('number')) 
+      return [undefined, groupsAxis1, groupsAxis2];
     let groupsAxis = (typeof(groupsAxis1) == 'number') ? groupsAxis1: groupsAxis2;
-    let labelsIds = DEH.get('axisForGraph')[+groupsAxis][DEH.getPositionOfAttr('structureAxisforgraph',  'labels')];
+    let labelsIds = DEH.get('axisForGraph')[+groupsAxis]
+      [DEH.getPositionOfAttr('structureAxisforgraph', 'labels')];
     groupsAxis = labelsIds.map(
-      (labelId:number) => DEH.get('labelForGraph')[labelId][DEH.getPositionOfAttr('structureLabelforgraph',  'label')]);
-    let colors = labelsIds.map((labelId:number) => DEH.get('labelForGraph')[labelId][DEH.getPositionOfAttr('structureLabelforgraph',  'color')]);
-    if (typeof(groupsAxis1) == 'number') groupsAxis1 = groupsAxis; else groupsAxis2 = groupsAxis;
+      (labelId:number) => DEH.get('labelForGraph')
+        [labelId][DEH.getPositionOfAttr('structureLabelforgraph', 'label')]);
+    let colors = labelsIds.map(
+      (labelId:number) => DEH.get('labelForGraph')
+        [labelId][DEH.getPositionOfAttr('structureLabelforgraph', 'color')]);
+    if (typeof(groupsAxis1) == 'number') groupsAxis1 = groupsAxis; 
+    else groupsAxis2 = groupsAxis;
     return [colors, groupsAxis1, groupsAxis2];
   }
-
-  updateCurrentNode(node:Node){
-    SliceDice.currentNode = node;
-    SliceDice.currentSlice = PDV.slice(node);
-  }
   
-  private fillUpWidget(dataWidget: DataWidget, axis1:string, axis2:string, indicator:string, 
-      addConditions:[string, number[]][]): void{
-    let newPdvs = (addConditions.length == 0) ? SliceDice.currentSlice: PDV.reSlice(SliceDice.currentSlice, addConditions);
-    let irregular: string = 'no';
+  private fillUpWidget(dataWidget: DataWidget, axis1:string, axis2:string, 
+    indicator:string, addConditions:[string, number[]][]): void{
+
+    let newPdvs = (addConditions.length == 0) ? SliceDice.currentSlice: 
+      PDV.reSlice(SliceDice.currentSlice, addConditions);
+    let irregular: 'no'|'line'|'col' = 'no';
     if (nonRegularAxis.includes(axis1)) irregular = 'line';
     else if (nonRegularAxis.includes(axis2)) irregular = 'col';
     for (let pdv of newPdvs){
@@ -125,12 +164,15 @@ export class SliceDice{
   }
 
   private ComputeAxisName(axis:string){
-    if (axis == 'lgp-1') return PDV.geoTree.attributes['natures'][1];
-    if (['lg-1', 'lt-1'].includes(axis)){
-      let childNature = SliceDice.currentNode.children[0] instanceof PDV ? 'site': (SliceDice.currentNode.children[0] as Node).nature;
-      return childNature;
+    switch(axis){
+      case 'lgp-1': return PDV.geoTree.attributes['natures'][1];
+      case 'lg-1': case 'lt-1':{
+        let childNature = SliceDice.currentNode.children[0] instanceof PDV ? 'site': 
+        (SliceDice.currentNode.children[0] as Node).nature;
+        return childNature;
+      }
+      default: return axis
     }
-    return axis
   }
 
   private computeAxis(axis:string){
@@ -148,19 +190,5 @@ export class SliceDice{
     let dataWidget = new DataWidget(rowsTitles, columnsTitles, idToI, idToJ);
     this.fillUpWidget(dataWidget, newAxis1, newAxis2, indicator, addConditions);
     return dataWidget;
-  }
-
-  rubiksCubeCheck(indicator: string, percent:string){
-    let sortLines = percent !== 'classic';
-    let dataWidget = this.getDataFromPdvs('enseigne', 'segmentMarketing', indicator.toLowerCase(), []);
-    dataWidget.widgetTreatement(false, sortLines, 'justLines');
-    return dataWidget.numberToBool()
-  }
-
-  updateTargetLevel(newValue: number, targetLevelName: string, targetLevelId: string, 
-      volumeid: number, targetLevelStructure: string){
-    let newTargetLevel: number[] = DEH.get(targetLevelName)[targetLevelId]
-    newTargetLevel[+DEH.get(targetLevelStructure).indexOf(volumeid)] = +newValue;
-    this.dataService.updateTargetLevel(newTargetLevel, targetLevelName as UpdateFields, +targetLevelId);
-  }
+  }  
 };
